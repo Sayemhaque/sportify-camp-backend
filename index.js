7b1345f58ed36aa45cf1aa09ceed5fe7b83dd237
sayemhaque <mdsayemofficial1000@gmail.com>
2023-06-08 18:34:20 +0600

    make admin and make instructor added

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const verifyJWT = (req,res,next) => {
 }
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@simple-crud-2023.h8uagaz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   serverApi: {
@@ -45,6 +45,7 @@ async function run() {
     const usersCollection = client.db('sportifyDB').collection('users');
     const classCollection = client.db('sportifyDB').collection('classes');
     
+      //jwt token
     app.post("/jwt", (req,res) => {
       const {email} = req.body;
       const token = jwt.sign({email} , process.env.JWT_SECRET,
@@ -62,6 +63,18 @@ async function run() {
       }
       next()
     }
+
+    //verify instructor
+    const verifyInstructor = async (req, res, next) => {
+      const email = req.decoded.email
+      const query = { email: email }
+      const user = await usersCollection.findOne(query)
+      if (user.role !== 'instructor') {
+        return res.status(403).send({ error: true, message: "forbiden" })
+      }
+      next()
+    }
+
     
     app.get('/' , (req,res) => {
         res.send("i am sportigy camp")
@@ -70,7 +83,7 @@ async function run() {
 
    
 
-    //jwt token
+  
     
 
     // create a new user
@@ -83,7 +96,40 @@ async function run() {
      const result = await usersCollection.insertOne(user)
      res.send(result)
     })
+   
+    // update student to admin
+    app.patch('/admin/:id' , async (req,res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)} 
+      const updatedDoc = {
+        $set:{
+          role:"admin"
+        }
+      }
+      const result = await usersCollection.updateOne(filter,updatedDoc)
+      console.log(res)
+    })
     
+    //update student to instructor
+    app.patch('/instructor/:id' , async (req,res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)} 
+      const updatedDoc = {
+        $set:{
+          role:"instructor"
+        }
+      }
+      const result = await usersCollection.updateOne(filter,updatedDoc)
+      console.log(result)
+    })
+
+    // get all the user as admin
+    app.get('/users' , async (req,res) => {
+      const result = await usersCollection.find().toArray()
+      res.send(result)
+    })
+    
+
     //verify admin role
     app.get('/user/admin/:email', verifyJWT, async (req,res) =>{
       const email = req.params.email;
@@ -95,6 +141,12 @@ async function run() {
       const user = await usersCollection.findOne(query)
       const result = {admin:user?.role === "admin"}
       console.log(result)
+      res.send(result)
+    })
+
+
+    app.get('/allclasses',verifyJWT,verifyAdmin, async (req,res) => { 
+      const result = await classCollection.find().toArray()
       res.send(result)
     })
 
@@ -118,12 +170,24 @@ async function run() {
 
  
     //add a class
-    app.post("/add/class" , verifyJWT,  async (req,res) => {
+    app.post("/add/class" , verifyJWT,  verifyInstructor, async (req,res) => {
        const data = req.body;
        const result = await classCollection.insertOne(data)
        res.send(result)
     })
   
+
+    // get all the classes added by instructor
+    app.get('/instructor/classes', verifyJWT,verifyInstructor, async(req,res) => {
+      const email = req.query.email;
+      if(req.decoded.email !== email){
+        return res.send({error:true,message:"unauthorized instructor"})
+      }
+      const query = {email:email}
+      const result = await classCollection.find(query).toArray()
+      res.send(result)
+     
+    })
 
 
     await client.db("admin").command({ ping: 1 });
